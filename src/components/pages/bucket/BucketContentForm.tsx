5254cb9fcc4e1dd425c7c06a5badcac2520fcd5f
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-06-02 14:45:27 -0300

    refactor: improve layout and structure of BucketContentForm component

--- src/components/pages/bucket/BucketContentForm.tsx
+++ src/components/pages/bucket/BucketContentForm.tsx
@@ -27,23 +27,27 @@ export const BucketContentForm = ({ refreshData, bucketName }: Props) => {
     };
 
     return (
+        <div>
+            <p className="text-2xl font-semibold">Upload new file</p>
+            <div className="flex items-center p-1 border-1 rounded-lg bg-gray-200 text-black">
 
-        <div className="flex items-center p-1 border-1 rounded-lg bg-gray-200 text-black">
-            <Input
-                type="file"
-                accept=".jpg, .png"
-                className="border-0 cursor-pointer  hover:bg-gray-400 transition duration-200 ease-in-out"
-                onChange={(e) => {
-                    const selectedFile = e.target.files?.[0];
-                    if (selectedFile) setFile(selectedFile);
-                }}
-                required
-            />
-            <Button variant={"secondary"} onClick={handleSubmit} disabled={!file}>
-                <Upload className="mr-3" />
-                Subir Arquivo
-            </Button>
+                <Input
+                    type="file"
+                    accept=".jpg, .png"
+                    className="border-0 cursor-pointer  hover:bg-gray-400 transition duration-200 ease-in-out"
+                    onChange={(e) => {
+                        const selectedFile = e.target.files?.[0];
+                        if (selectedFile) setFile(selectedFile);
+                    }}
+                    required
+                />
+                <Button variant={"secondary"} onClick={handleSubmit} disabled={!file}>
+                    <Upload className="mr-3" />
+                    Upload
+                </Button>
+            </div>
         </div>
+
 
     )
 
